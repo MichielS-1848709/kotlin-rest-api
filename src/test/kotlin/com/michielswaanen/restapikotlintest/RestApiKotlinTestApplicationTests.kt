aa94d08a002939af01a3d9c83710cf0e1eeb9082
Michiel Swaanen <michiel.swaanen.gsm@gmail.com>
2021-01-24 18:27:58 +0100

    :tada: Kotlin Rest API Adventure with Spring

---
+++ src/test/kotlin/com/michielswaanen/restapikotlintest/RestApiKotlinTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.michielswaanen.restapikotlintest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestApiKotlinTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
